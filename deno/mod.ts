c0e8b9e4fc3e6f10b6bb33c79e3a4872f032dda9
Asher Gomez <ashersaupingomez@gmail.com>
2024-03-20 13:20:03 +1100

    feat(deno): `Config` interface

---
+++ deno/mod.ts
@@ -0,0 +1,15 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+/**
+ * Tools for working with {@link https://deno.land/api | Deno runtime APIs}.
+ *
+ * ```ts
+ * import type { Config } from "https://deno.land/std@$STD_VERSION/deno/types.ts";
+ *
+ * function readConfig(config: Config) {
+ *   // ...
+ * }
+ * ```
+ */
+export * from "./types.ts";
